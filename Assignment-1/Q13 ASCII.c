9eb0a5cfa444759ccd940181e5ce445f20292372
Nitya Shah <ncs110905@gmail.com>
2025-08-31 16:28:23 +0530

    ASCII VALUE

---
+++ Assignment-1/Q13 ASCII.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    // %d displays the integer value of the character
+    printf("ASCII value of %c = %d\n", c, c);
+
+    return 0;
+}
+
