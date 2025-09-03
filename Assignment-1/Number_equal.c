e8305b3a37eeb7d24762f4e18bafd1266ac11ca5
Nitya Shah <ncs110905@gmail.com>
2025-09-03 23:17:16 +0530

    Equal Numbers

---
+++ Assignment-1/Number_equal.c
@@ -0,0 +1,17 @@
+// Write a C program to accept two integers and check whether they 
+// are equal or not 
+
+#include<stdio.h>
+
+int main (){
+    int num1 , num2;
+    printf("Enter two numbers : ");
+
+    scanf("%d %d",&num1,&num2);
+    if(num1 == num2){
+        printf("%d and %d are equal.",num1,num2);
+    }
+    else{
+        printf("%d and %d are not equal.",num1,num2);
+    }
+}
