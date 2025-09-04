138c0e0c505cfce11373f1d560e4881a5e26f44e
Nitya Shah <ncs110905@gmail.com>
2025-09-04 23:49:03 +0530

    Min of three

---
+++ Assignment-1/Min_Among_Three.c
@@ -0,0 +1,13 @@
+// WAP to find minimum number among 3 
+// numbers using ternary operator
+
+#include<stdio.h>
+
+int main(){
+    int num1 , num2 , num3;
+
+    printf("Enter three numbers : ");
+    scanf("%d %d %d",&num1,&num2,&num3);
+
+    (num1<num2) ? ((num1<num3)?printf("%d",num1):printf("%d",num3)) : ((num2<num3)?printf("%d",num2) : printf("%d",num3)); 
+}
