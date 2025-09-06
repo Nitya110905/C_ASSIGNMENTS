317187f69c21985635fcbf13767ff722492ad7fd
Nitya Shah <ncs110905@gmail.com>
2025-09-06 23:13:53 +0530

    Adding digits

---
+++ Assignment-1/Sum_of_digits.c
@@ -0,0 +1,18 @@
+// Write a program make a summation of given number (E.g., 
+// 1523 Ans: -11)
+
+#include<stdio.h>
+
+int main(){
+    int sum = 0 , rem , num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    while(num > 0){
+        rem = num % 10;
+        sum = sum + rem;
+        num = num/10;
+    }
+    printf("%d",sum);
+
+    return 0;
+}
