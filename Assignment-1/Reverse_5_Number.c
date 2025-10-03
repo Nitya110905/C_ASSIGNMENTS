4a06cf0fb8097038a1a9fa30a5954c2e3f6f4ca4
Nitya Shah <ncs110905@gmail.com>
2025-10-03 22:37:27 +0530

    Reverse 5 numbers

--- Assignment-1/Reverse_5_Number.c
+++ Assignment-1/Reverse_5_Number.c
@@ -8,17 +8,17 @@ int main() {
     int num[5], remainder;
 
     for(int i = 0 ; i < 5 ; i++){
-    printf("Enter integer %d to reverse: ",i+1);
-    scanf("%d", &num[i]);
+        printf("Enter integer %d to reverse: ",i+1);
+        scanf("%d", &num[i]);
 
-    int reversed_num = 0;
-    for (; num[i] != 0; num[i] /= 10) {
-        remainder = num[i] % 10;
+        int reversed_num = 0;
+        for (; num[i] != 0; num[i] /= 10) {
+            remainder = num[i] % 10;
         
-        reversed_num = reversed_num * 10 + remainder;
-    }
+            reversed_num = reversed_num * 10 + remainder;
+        }
 
-    printf("Reversed number = %d\n", reversed_num);
+        printf("Reversed number = %d\n", reversed_num);
     }
 
     return 0;
