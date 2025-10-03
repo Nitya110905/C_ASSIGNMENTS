8b41e2d96c9b92d4f3068346d110333daa972c77
Nitya Shah <ncs110905@gmail.com>
2025-10-03 22:13:08 +0530

    Reverse 5 numbers

---
+++ Assignment-1/Reverse_5_Number.c
@@ -0,0 +1,25 @@
+//  WAP to accept 5 numbers from user and display in reverse 
+// order using for loop and array 
+
+
+#include <stdio.h>
+
+int main() {
+    int num[5], remainder;
+
+    for(int i = 0 ; i < 5 ; i++){
+    printf("Enter integer %d to reverse: ",i+1);
+    scanf("%d", &num[i]);
+
+    int reversed_num = 0;
+    for (; num[i] != 0; num[i] /= 10) {
+        remainder = num[i] % 10;
+        
+        reversed_num = reversed_num * 10 + remainder;
+    }
+
+    printf("Reversed number = %d\n", reversed_num);
+    }
+
+    return 0;
+}
