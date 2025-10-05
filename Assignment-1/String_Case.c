e59fb22ccff0ea9c3d5d193660ef5c30bd9cfdb8
Nitya Shah <ncs110905@gmail.com>
2025-10-05 14:30:51 +0530

    Upper-Lower Case

---
+++ Assignment-1/String_Case.c
@@ -0,0 +1,28 @@
+//  Write a program in C to read a sentence and replace 
+// lowercase characters with uppercase and vice versa.
+
+#include <stdio.h>
+#include <ctype.h> 
+
+int main() {
+    char str[150];
+    int i = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        if (islower(str[i])) {
+            str[i] = toupper(str[i]);
+        }
+        else if (isupper(str[i])) {
+            str[i] = tolower(str[i]);
+        }
+
+        i++;
+    }
+
+    printf("String with swapped case: %s", str);
+
+    return 0;
+}
